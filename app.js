c78d19ad2575a3a09652c1933677816059087692
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-29 15:33:57 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// importo express
+const express = require('express');
+
+// inizializzo express in una variabile
+const app = express();
+
+// numero di porta
+const port = 3000;
+
+// uso i middleware per gli asset statici
+app.use(express.static('public'));
+
+// middleware per il parsing del body req
+app.use(express.json());
+
+// definisco l'entry point
+app.get('/', (req, res) => {
+    res.send('Books API server')
+});
+
+// metto in ascolto il server
+app.listen(port, () => {
+    console.log('server in ascolto alla porta 3000')
+});
